5d49c6996901a2877557557ec02ed298687fdd09
Alex Van Camp <vancamp.alex@gmail.com>
2018-05-23 15:27:52 -0500

    docs: add comment to validateSignatureHeader
    [skip ci]

--- src/security.ts
+++ src/security.ts
@@ -5,12 +5,17 @@ import * as crypto from 'crypto';
  * your app Consumer Secret.
  * @param crcToken - The token provided by the incoming GET request.
  * @param consumerSecret - The consumer secret of this application from apps.twitter.com.
- * @return string
  */
 export function getChallengeResponse(crcToken: string, consumerSecret: string) {
 	return crypto.createHmac('sha256', consumerSecret).update(crcToken).digest('base64');
 }
 
+/**
+ * See https://developer.twitter.com/en/docs/accounts-and-users/subscribe-account-activity/guides/securing-webhooks
+ * @param bodyPayload - The raw string body of the incoming webhook POST request.
+ * @param consumerSecret - The consumer secret of this application from apps.twitter.com.
+ * @param headerDigest - The digest provided by the x-twitter-webhooks-signature on the request.
+ */
 export function validateSignatureHeader(bodyPayload: string, consumerSecret: string, headerDigest: string | undefined) {
 	if (!headerDigest) {
 		return false;
